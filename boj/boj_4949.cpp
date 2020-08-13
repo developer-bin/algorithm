20756ad9871aa25f2be674c03d01b6340fe12109
developer-bin <galaxys7lub1158@gmail.com>
2020-08-13 22:55:18 +0900

    Create boj_4949.cpp

---
+++ boj/boj_4949.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define MOD 9901
+#define ll long long
+using namespace std;
+string s;
+bool checking(stack<char> &st, char c) {
+	if (st.empty()) {
+		return false;
+	}
+	if (st.top() == c) {
+		st.pop();
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	while (true) {
+		//input
+		getline(cin, s);
+		if (s == ".")break;
+		//solve
+		stack<char> st;
+		bool check = true;
+		for (int i = 0; i < (int)s.length(); i++) {
+			if (s[i] == '(' || s[i] == '[') {
+				st.push(s[i]);
+			}
+			else if (s[i] == ')') {
+				if (checking(st, '(') == false) {
+					check = false;
+					break;
+				}
+			}
+			else if (s[i] == ']') {
+				if (checking(st, '[') == false) {
+					check = false;
+					break;
+				}
+			}
+		}
+		if (st.empty() == false || check == false) {
+			cout << "no\n";
+		}
+		else if (check) {
+			cout << "yes\n";
+		}
+	}
+
+}
