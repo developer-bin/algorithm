b29b71702892c0e85fa68a94e71e5d0a138e11d4
developer-bin <galaxys7lub1158@gmail.com>
2020-11-02 21:48:48 +0900

    Create boj_15651.cpp

---
+++ boj/boj_15651.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define MOD 9901
+#define ll long long
+using namespace std;
+int n, m;
+bool visit[9];
+vector<int> permu_v;
+void permu(int cnt) {
+	if (cnt == m) {
+		for (int k : permu_v) {
+			cout << k << ' ';
+		}cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		permu_v.push_back(i);
+		permu(cnt + 1);
+		permu_v.pop_back();
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n >> m;
+	//solve
+	permu(0);
+}
