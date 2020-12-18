3d7cbefcc26faa95d27b049146b3911718999e03
Woobin Lee <galaxys7lub1158@gmail.com>
2020-12-18 21:20:33 +0900

    Create boj_1158.cpp

---
+++ boj/boj_1158.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n, k;
+queue<int> q;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+	//solve
+	cout << '<';
+	int cnt = 1;
+	while (!q.empty()) {
+		int num = q.front();
+		q.pop();
+		if (cnt == k) {
+			cout << num;
+			cnt = 1;
+			if (q.empty() == false) {
+				cout << ", ";
+			}
+			continue;
+		}
+		cnt += 1;
+		q.push(num);
+	}
+	cout << '>';
+	//print
+}
