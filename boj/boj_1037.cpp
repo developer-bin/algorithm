4d5b3d97e8318153d1527a39f0d0e5acb52f4f45
developer-bin <galaxys7lub1158@gmail.com>
2020-12-25 08:17:37 +0900

    Create boj_1037.cpp

---
+++ boj/boj_1037.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int n;
+int min_num=MAX_VALUE;
+int max_num = MIN_VALUE;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		min_num = min(min_num, num);
+		max_num = max(max_num, num);
+	}
+	//solve
+	//print
+	cout << min_num * max_num;
+}
