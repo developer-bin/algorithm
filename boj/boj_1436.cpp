017cd34dab16123f1b1797a1b9b008ead4408364
developer-bin <galaxys7lub1158@gmail.com>
2020-12-27 15:39:44 +0900

    Create boj_1436.cpp

---
+++ boj/boj_1436.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int n;
+int num = 666;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n-1; i++) {
+		while (true) {
+			num += 1;
+			string s = to_string(num);
+			if (s.find("666", 0, 3) != string::npos) break;
+		}
+	}
+	//print
+	cout << num;
+}
