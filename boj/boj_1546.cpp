8ae309ed1093bd725607c995ffa8416b6592e0f0
Woobin Lee <galaxys7lub1158@gmail.com>
2021-01-21 22:00:02 +0900

    Create boj_1546.cpp

---
+++ boj/boj_1546.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+vector<double> v;
+double max_num = MIN_VALUE;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		double num;
+		cin >> num;
+		v.push_back(num);
+		max_num = max(max_num, num);
+	}
+	//solve
+	for (int i = 0; i < v.size(); i++) {
+		v[i] = v[i] / max_num * 100;
+	}
+	//print
+	double ans = 0;
+	for (int i = 0; i < v.size(); i++) {
+		ans += v[i];
+	}
+	cout << ans / v.size();
+}
