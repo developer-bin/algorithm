656a0c552c36dc30d0d4c87b0d2cfff06373a564
Woobin Lee <galaxys7lub1158@gmail.com>
2021-02-15 22:11:46 +0900

    Create boj_2577.cpp

---
+++ boj/boj_2577.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int a, b, c;
+int ans;
+int cnt[10];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> a >> b >> c;
+	//solve
+	ans = a * b * c;
+	string s = to_string(ans);
+	for (int i = 0; i < (int)s.length(); i++) {
+		cnt[s[i] - '0'] += 1;
+	}
+	//print
+	for (int i = 0; i < 10; i++) {
+		cout << cnt[i] << '\n';
+	}
+	return 0;
+}
