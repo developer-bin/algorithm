1bbd1c04ca9386963d83bf675aad6586c70b8672
Woobin Lee <galaxys7lub1158@gmail.com>
2021-02-26 00:26:47 +0900

    Create boj_2743.cpp

---
+++ boj/boj_2743.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+string s;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> s;
+	//solve
+	//print
+	cout << (int)s.length();
+	return 0;
+}
