88119d94a8a134ea48954e97a5f7ff1fddd44c60
developer-bin <galaxys7lub1158@gmail.com>
2021-03-01 13:26:40 +0900

    Create boj_7567.cpp

---
+++ boj/boj_7567.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+string s;
+int ans = 10;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> s;
+	//solve
+	for (int i = 1; i < (int)s.length(); i++) {
+		if (s[i] != s[i - 1])ans += 10;
+		else ans += 5;
+	}
+	cout << ans;
+}
