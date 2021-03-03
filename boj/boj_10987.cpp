6d702de8d40dd176c6832ab4c273c7edc5431bc0
Woobin Lee <galaxys7lub1158@gmail.com>
2021-03-04 00:25:27 +0900

    Create boj_10987.cpp

---
+++ boj/boj_10987.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+string s;
+int cnt;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> s;
+	//solve
+	for (int i = 0; i < (int)s.length(); i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+			cnt += 1;
+		}
+	}
+	//print
+	cout << cnt;
+	return 0;
+}
