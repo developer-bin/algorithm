4b86d73481323b5be1895413a053eab0da0ec321
developer-bin <galaxys7lub1158@gmail.com>
2021-03-14 10:56:59 +0900

    Create boj_2605.cpp

---
+++ boj/boj_2605.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int n;
+vector<int> v;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		if (num == 0) {
+			v.push_back(i);
+		}
+		else {
+			v.insert(v.begin()+(i - 1 - num),i);
+		}
+	}
+	//solve
+	for (int k : v) {
+		cout << k << ' ';
+	}
+}
