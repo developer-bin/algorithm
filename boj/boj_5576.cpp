ed136ea5a417359433bd642ab827f9241c987b20
Woobin Lee <galaxys7lub1158@gmail.com>
2021-03-25 00:03:50 +0900

    Create boj_5576.cpp

---
+++ boj/boj_5576.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+vector<int> v;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	for (int i = 1; i <= 10; i++) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	cout << v[v.size() - 1] + v[v.size() - 2] + v[v.size() - 3]<<' ';
+	v.clear();
+	for (int i = 1; i <= 10; i++) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	cout << v[v.size() - 1] + v[v.size() - 2] + v[v.size() - 3] << ' ';
+	//solve
+	//print
+}
