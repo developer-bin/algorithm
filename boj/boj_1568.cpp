5ef3d55472e50e26987bb030b32715a9804a1999
Woobin Lee <galaxys7lub1158@gmail.com>
2021-03-27 01:06:00 +0900

    Create boj_1568.cpp

---
+++ boj/boj_1568.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+int times;
+int diff = 1;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	while (n!=0) {
+		if (n < diff)diff = 1;
+		n -= diff;
+		times += 1;
+		diff += 1;
+	}
+	//print
+	cout << times;
+}
