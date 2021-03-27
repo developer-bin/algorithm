2a9fc8e460af2fc5aa080695bcd05b619a424562
developer-bin <galaxys7lub1158@gmail.com>
2021-03-28 00:21:53 +0900

    Create boj_10822.cpp

---
+++ boj/boj_10822.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+stringstream ss;
+string s;
+string slice_s;
+int ans;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> s;
+	ss.str(s);
+	//solve
+	while (getline(ss, slice_s, ',')) {
+		ans += stoi(slice_s);
+	}
+	cout << ans;
+}
