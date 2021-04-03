d48ddd3dc31802e8064f5b0cc80ec2ed0f4f73af
Woobin Lee <galaxys7lub1158@gmail.com>
2021-04-04 00:49:47 +0900

    Create boj_10813.cpp

---
+++ boj/boj_10813.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n, m;
+int a[101];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	for (int i = 1; i <= 100; i++) {
+		a[i] = i;
+	}
+	//input
+	cin >> n >> m;
+	//solve
+	for (int i = 1; i <= m; i++) {
+		int from, to;
+		cin >> from >> to;
+		swap(a[from], a[to]);
+	}
+	//print
+	for (int i = 1; i <= n; i++) {
+		cout << a[i] << ' ';
+	}
+}
