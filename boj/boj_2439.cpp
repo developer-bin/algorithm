161dda19066f2e2395293bb8a7e20e430a65d6f3
developer-bin <galaxys7lub1158@gmail.com>
2021-04-10 17:41:55 +0900

    Create boj_2439.cpp

---
+++ boj/boj_2439.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int n;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			cout << ' ';
+		}
+		for (int j = n - i + 1; j <= n; j++) {
+			cout << '*';
+		}cout << "\n";
+	}
+}
