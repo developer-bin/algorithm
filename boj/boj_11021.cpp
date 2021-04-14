49de6a48bf3ce6cce9e1158737f17beab951d662
Woobin Lee <galaxys7lub1158@gmail.com>
2021-04-15 00:02:44 +0900

    Create boj_11021.cpp

---
+++ boj/boj_11021.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int t;
+int a, b;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> t;
+	//solve
+	for (int i = 1; i <= t; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << '\n';
+	}
+	//print
+}
