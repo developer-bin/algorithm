e5f9c56917afd1a207855cd3374c35445d99b6c0
Woobin Lee <galaxys7lub1158@gmail.com>
2021-04-27 00:35:00 +0900

    Create boj_2522.cpp

---
+++ boj/boj_2522.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n; i++) {
+		for (int j = n - i; j >= 1; j--) {
+			cout << ' ';
+		}
+		for (int j = 1; j <= i; j++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << ' ';
+		}
+		for (int j = n - i; j >= 1; j--) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	//print
+}
