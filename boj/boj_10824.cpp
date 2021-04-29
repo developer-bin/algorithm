ac2776be94b2ae6c40e76dc8713cb346f4782cb7
Woobin Lee <galaxys7lub1158@gmail.com>
2021-04-30 00:18:31 +0900

    Create boj_10824.cpp

---
+++ boj/boj_10824.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+string a, b, c, d;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> a >> b >> c >> d;
+	//solve
+	//print
+	cout << stoll(a + b) + stoll(c + d);
+}
