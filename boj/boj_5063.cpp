026f0d9b2a8b91183ede29e1fab23687cf89bf1e
Woobin Lee <galaxys7lub1158@gmail.com>
2021-05-04 00:19:25 +0900

    Create boj_5063.cpp

---
+++ boj/boj_5063.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+int r, e, c;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n; i++) {
+		cin >> r >> e >> c;
+		if (r < e - c)cout << "advertise\n";
+		else if (r > e - c)cout << "do not advertise\n";
+		else cout << "does not matter\n";
+	}
+	//print
+}
