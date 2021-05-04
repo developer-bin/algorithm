5a9dfa74e59c83351b997175a9ab3fff9abfad0a
Woobin Lee <galaxys7lub1158@gmail.com>
2021-05-05 00:05:41 +0900

    Create boj_10990.cpp

---
+++ boj/boj_10990.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			cout << ' ';
+		}
+		cout << '*';
+		for (int j = 1; j <= 2 * (i - 1) - 1; j++) {
+			cout << ' ';
+		}
+		if (i == 1) {
+			cout << "\n";
+			continue;
+		}
+		cout << "*\n";
+	}
+	//print
+}
