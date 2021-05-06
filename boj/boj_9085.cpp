6b870592a8fe51280326a18f5cd07837d32b5232
Woobin Lee <galaxys7lub1158@gmail.com>
2021-05-07 00:23:24 +0900

    Create boj_9085.cpp

---
+++ boj/boj_9085.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+int t;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> t;
+	//solve
+	while (t--) {
+		cin >> n;
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			int num;
+			cin >> num;
+			sum += num;
+		}
+		cout << sum << '\n';
+	}
+	//print
+}
