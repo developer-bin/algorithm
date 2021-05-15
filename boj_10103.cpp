ea698d2748835f560ada54087ffd8c97a15ddd95
developer-bin <galaxys7lub1158@gmail.com>
2021-05-16 00:08:41 +0900

    Create boj_10103.cpp

---
+++ boj_10103.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int n;
+int chang=100, sang=100;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	while (n--) {
+		int a, b;
+		cin >> a >> b;
+		if (a > b) {
+			sang -= a;
+		}
+		else if (a < b) {
+			chang -= b;
+		}
+	}
+	cout << chang << '\n' << sang;
+}
