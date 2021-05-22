88fd6c1331335de3ea99a2e3fe0686769f1b5c5f
developer-bin <galaxys7lub1158@gmail.com>
2021-05-22 22:44:06 +0900

    Create boj_13752.cpp

---
+++ boj/boj_13752.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int n;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		for (int j = 1; j <= num; j++) {
+			cout << "=";
+		}cout << '\n';
+	}
+}
