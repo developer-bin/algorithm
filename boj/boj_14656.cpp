0a26f0e3bad4de71355444b4597398741a24ca10
Woobin Lee <galaxys7lub1158@gmail.com>
2021-05-24 00:02:29 +0900

    Create boj_14656.cpp

---
+++ boj/boj_14656.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+int ans;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		if (i != num)ans += 1;
+	}
+	//print
+	cout << ans;
+}
