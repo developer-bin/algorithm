432f2b0d632f452075515abf9045a01d684028cf
Woobin Lee <galaxys7lub1158@gmail.com>
2021-06-08 00:17:14 +0900

    Create boj_10707.cpp

---
+++ boj/boj_10707.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int a, b, c, d, p;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> a >> b >> c >> d >> p;
+	//solve
+	int y = 0;
+	if (c < p) {
+		y = b + (p - c) * d;
+	}
+	else y = b;
+	cout << min(a * p, y);
+	//print
+}
