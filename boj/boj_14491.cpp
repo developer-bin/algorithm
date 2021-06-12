0012e2e24f4e230bec9f64dbc8631144da6c4f1b
developer-bin <galaxys7lub1158@gmail.com>
2021-06-13 00:06:08 +0900

    Create boj_14491.cpp

---
+++ boj/boj_14491.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int t;
+stack<int> st;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> t;
+	//solve
+	int mok = t;
+	while (true) {
+		int na = mok % 9;
+		mok = mok / 9;
+		st.push(na);
+		if (mok == 0) {
+			break;
+		}
+	}
+	while (!st.empty()) {
+		cout << st.top();
+		st.pop();
+	}
+}
