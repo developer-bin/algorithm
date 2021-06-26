f315ffcbfa163a64c5927584d80b75f90c1707fb
developer-bin <galaxys7lub1158@gmail.com>
2021-06-27 01:23:14 +0900

    Create boj_4470.cpp

---
+++ boj/boj_4470.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <unordered_map>
+#include <sstream>
+#define MIN_VALUE -2000000000
+#define MAX_VALUE 2000000000
+#define MOD 1000000000
+#define ll long long
+using namespace std;
+int n;
+string s;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	cin.ignore();
+	for (int i = 1; i <= n; i++) {
+		getline(cin, s);
+		cout << i << ". " << s << "\n";
+	}
+	//solve
+}
