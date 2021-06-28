dacca020dd88797dbb4e26781d9055fff64746ae
Woobin Lee <galaxys7lub1158@gmail.com>
2021-06-29 00:11:48 +0900

    Create boj_5789.cpp

---
+++ boj/boj_5789.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+int n;
+string s;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//init
+	//input
+	cin >> n;
+	//solve
+	for (int i = 1; i <= n; i++) {
+		cin >> s;
+		int len = (int)s.length();
+		if (s[len / 2 - 1] == s[len / 2])cout << "Do-it\n";
+		else cout << "Do-it-Not\n";
+	}
+}
