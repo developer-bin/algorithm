eec398976b922437029ff6076fdaf5eacc4b800e
Woobin Lee <galaxys7lub1158@gmail.com>
2021-08-18 21:10:43 +0900

    Create boj_11441.cpp

---
+++ boj/boj_11441.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+
+int n;
+int a[100001];
+int m;
+int sum[100001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		sum[i] = sum[i - 1] + a[i];
+	}
+	cin >> m;
+	for (int i = 1; i <= m; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << sum[b] - sum[a - 1]<<'\n';
+	}
+}
