2612293aa2c5b89b040aedab3e9b3309502c96c9
Woobin Lee <galaxys7lub1158@gmail.com>
2021-09-13 22:30:04 +0900

    Create boj_2083.cpp

---
+++ boj/boj_2083.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true) {
+		string s;
+		int age;
+		int weight;
+
+		cin >> s >> age >> weight;
+
+		if (s=="#" && age == 0 && weight == 0)break;
+
+		if (age > 17 || weight >= 80) {
+			cout << s << ' ' << "Senior\n";
+		}
+		else {
+			cout << s << ' ' << "Junior\n";
+		}
+	}
+}
