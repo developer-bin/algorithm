420ec2bcd8047ac6dfe07366c492b96334f34926
Woobin Lee <galaxys7lub1158@gmail.com>
2021-10-15 23:47:56 +0900

    Create boj_10173.cpp

---
+++ boj/boj_10173.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+
+string s;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true) {
+		getline(cin, s);
+
+		if (s == "EOI")break;
+
+		for (int i = 0; i < (int)s.length(); i++) {
+			s[i] = toupper(s[i]);
+		}
+		
+		if (string::npos == s.find("NEMO"))cout << "Missing\n";
+		else cout << "Found\n";
+	}
+}
