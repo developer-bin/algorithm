43a465c49753c2e843c90695068652be1ff1b373
Woobin Lee <galaxys7lub1158@gmail.com>
2021-11-06 20:42:29 +0900

    Create boj_4806.cpp

---
+++ boj/boj_4806.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+
+string s;
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (getline(cin, s)) {
+		n += 1;
+	}
+
+	cout << n;
+}
