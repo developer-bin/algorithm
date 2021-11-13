f5f54d6220b740df185c80348a4b519466f4dc51
Woobin Lee <galaxys7lub1158@gmail.com>
2021-11-13 11:29:19 +0900

    Create boj_10718.cpp

---
+++ boj/boj_10718.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <stack>
+#include <list>
+#define MIN_VALUE -200000000
+#define MAX_VALUE 200000000
+#define ll long long
+using namespace std;
+
+string s;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "강한친구 대한육군\n";
+	cout << "강한친구 대한육군\n";
+}
