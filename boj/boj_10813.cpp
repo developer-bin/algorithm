f4ebcc31fe5c9dc276b7eb64a94ebf36307ef054
Woobin Lee <galaxys7lub1158@gmail.com>
2022-01-15 18:53:21 +0900

    Update boj_10813.cpp

--- boj/boj_10813.cpp
+++ boj/boj_10813.cpp
@@ -18,10 +18,12 @@
 using namespace std;
 int n, m;
 int a[101];
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
 	//init
 	for (int i = 1; i <= 100; i++) {
 		a[i] = i;
