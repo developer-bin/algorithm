836aa83462200e614972224d60780f49ed4dcca8
Woobin Lee <galaxys7lub1158@gmail.com>
2022-02-23 23:06:25 +0900

    Update boj_10824.cpp

--- boj/boj_10824.cpp
+++ boj/boj_10824.cpp
@@ -17,10 +17,12 @@
 #define ll long long
 using namespace std;
 string a, b, c, d;
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
 	//init
 	//input
 	cin >> a >> b >> c >> d;
