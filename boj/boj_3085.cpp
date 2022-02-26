34b1b6b4dc43a503ee006516c1cab01cb39141b1
Woobin Lee <galaxys7lub1158@gmail.com>
2022-02-26 12:14:45 +0900

    Update boj_3085.cpp

--- boj/boj_3085.cpp
+++ boj/boj_3085.cpp
@@ -61,11 +61,13 @@ void checking() {
 }
 
 int main() {
+
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	cin >> n;
+
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			cin >> a[i][j];
