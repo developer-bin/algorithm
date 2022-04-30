71e42708942c37facaf401b3bd6983bf670e2f9d
Woobin Lee <galaxys7lub1158@gmail.com>
2022-04-30 22:57:56 +0900

    Update boj_2920.cpp

--- boj/boj_2920.cpp
+++ boj/boj_2920.cpp
@@ -16,10 +16,12 @@
 #define ll long long
 using namespace std;
 vector<int> v;
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
 	//init
 	//input
 	for (int i = 1; i <= 8; i++) {
