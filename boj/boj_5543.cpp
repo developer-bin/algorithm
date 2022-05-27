f87867d43d45d030605d576f2be75db08ef24bca
Woobin Lee <galaxys7lub1158@gmail.com>
2022-05-27 23:28:15 +0900

    Update boj_5543.cpp

--- boj/boj_5543.cpp
+++ boj/boj_5543.cpp
@@ -18,10 +18,12 @@
 using namespace std;
 int buger1, buger2, buger3;
 int cola, sida;
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
 	//init
 	//input
 	cin >> buger1 >> buger2 >> buger3 >> cola >> sida;
