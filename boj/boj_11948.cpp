4a49c1cd1cf23ea3043c701d448adcd5d01299b8
Woobin Lee <galaxys7lub1158@gmail.com>
2022-08-06 23:45:48 +0900

    Update boj_11948.cpp

--- boj/boj_11948.cpp
+++ boj/boj_11948.cpp
@@ -18,10 +18,12 @@
 using namespace std;
 int a, b, c, d, e, f;
 int sum;
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
 	//init
 	//input
 	cin >> a >> b >> c >> d >> e >> f;
