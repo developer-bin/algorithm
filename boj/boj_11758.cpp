e6523f1ab2dbb2e7f0edde7339c5829152afe501
Woobin Lee <galaxys7lub1158@gmail.com>
2022-08-25 23:37:45 +0900

    Update boj_11758.cpp

--- boj/boj_11758.cpp
+++ boj/boj_11758.cpp
@@ -6,6 +6,7 @@
 #define MIN_VALUE -200000000
 #define MAX_VALUE 200000000
 using namespace std;
+
 int ccw(int x1, int y1, int x2, int y2,int x3,int y3) {
 	int result= x1 * y2 + x2 * y3 + x3 * y1 - (x1*y3 + x2 * y1 + x3 * y2);
 	if (result == 0)return 0;
